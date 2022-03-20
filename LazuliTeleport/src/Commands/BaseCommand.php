49b21d9b98e6978987da1cda5cd5eb822bdf44ea
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-21 05:14:02 +0800

    Added a plugin base command

---
+++ LazuliTeleport/src/Commands/BaseCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\LazuliTeleport\Commands;
+
+use CortexPE\Commando\args\BaseArgument;
+use CortexPE\Commando\BaseCommand as CommandoBaseCommand;
+use Generator;
+use pocketmine\command\CommandSender;
+use SOFe\AwaitGenerator\Await;
+
+abstract class BaseCommand extends CommandoBaseCommand
+{
+    protected function prepare() : void
+    {
+    }
+
+    /**
+     * @param array<string, BaseArgument> $args
+     */
+    final public function onRun(
+        CommandSender $sender,
+        string $aliasUsed,
+        array $args
+    ) : void {
+        $reject = [
+            InGameCommandException::class => fn(InGameCommandException $err) => $sender->sendMessage($err->getMessage())
+            // TODO: DisposableException::class
+        ];
+        Await::g2c($this->asyncRun(
+            $sender,
+            $aliasUsed,
+            $args
+        ), [], $reject);
+    }
+
+    /**
+     * @param array<string, BaseArgument> $args
+     * @throws InGameCommandException
+     * @return Generator<mixed, mixed, mixed, void>
+     */
+    abstract protected function asyncRun(
+        CommandSender $sender,
+        string $aliasUsed,
+        array $args
+    ) : Generator;
+}
