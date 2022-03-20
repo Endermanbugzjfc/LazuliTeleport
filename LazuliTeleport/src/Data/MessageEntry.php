6ad0d6cfb538a5e8075488549a2457962e3f333f
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-20 22:12:13 +0800

    Added message entry create functions

--- LazuliTeleport/src/Data/MessageEntry.php
+++ LazuliTeleport/src/Data/MessageEntry.php
@@ -13,4 +13,30 @@ class MessageEntry
     public ?int $fadeIn = null;
     public ?int $stay = null;
     public ?int $fadeOut = null;
+
+    public static function createChat(
+        string $content
+    ) : self {
+        $self = new self();
+        $self->chat = $content;
+        return $self;
+    }
+
+    public static function createTitle(
+        string $content,
+        string $subtitle = ""
+    ) : self {
+        $self = new self();
+        $self->title = $content;
+        $self->subtitle = $content;
+        return $self;
+    }
+
+    public static function createActionbar(
+        string $content
+    ) : self {
+        $self = new self();
+        $self->actionbar = $content;
+        return $self;
+    }
 }
