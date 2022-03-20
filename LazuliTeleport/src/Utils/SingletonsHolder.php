2d7c36221727942e42c73045cd937bb47f70f479
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-21 06:04:37 +0800

    Added singletons holder

---
+++ LazuliTeleport/src/Utils/SingletonsHolder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Endermanbugzjfc\LazuliTeleport\Utils;
+
+class SingletonsHolder {
+
+	/**
+	 * @var array<class-string, object>
+	 */
+	protected array $singletons = [];
+
+	public function register(
+		object $singleton
+	) : void {
+		$class = $singleton::class;
+		$this->singletons[$class] = $singleton;
+	}
+
+	/**
+	 * @template T of object
+	 * @param class-string<T> $class
+	 * @return T
+	 */
+	public function get(
+		string $class
+	) : object {
+		/**
+		 * @var T
+		 */
+		$object = $this->singletons[$class];
+		return $object;
+	}
+}
