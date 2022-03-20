a43e7dc4df0d4bf28fc57bc206e79c5fcd9cd62a
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-20 21:52:43 +0800

    Register more permissions

--- LazuliTeleport/src/LazuliTeleport.php
+++ LazuliTeleport/src/LazuliTeleport.php
@@ -27,10 +27,15 @@ class LazuliTeleport extends PluginBase
         $config = new PluginConfig();
         $context = Parse::object($config, $data);
         $context->copyToObject($config, $path);
-        $description = "LazuliTeleport wait time permission";
+
+        $waitTimeDescription = "LazuliTeleport wait time permission";
         foreach ($config->waitTimeAfterAcceptRequest as $permission => $time) {
-            $permissionInstance = new Permission($permission, $description);
+            $permissionInstance = new Permission($permission, $waitTimeDescription);
             PermissionManager::getInstance()->addPermission($permissionInstance);
+        }
+        $requesteeLimitDescription = "LazuliTeleport tpahere requestee limit permission";
+        foreach ($config->tpahereRequesteeLimit as $permission => $limit) {
+            $permissionInstance = new Permission($permission, $requesteeLimitDescription);
         }
     }
 
