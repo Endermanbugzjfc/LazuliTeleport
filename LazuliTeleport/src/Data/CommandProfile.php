1546d242747f27d2a02784a4c7f2d43d3f68ffbf
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-20 18:48:57 +0800

    Added command profile

---
+++ LazuliTeleport/src/Data/CommandProfile.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Endermanbugzjfc\LazuliTeleport\Data;
+
+class CommandProfile
+{
+    public string $name;
+
+    public string $description;
+
+    /**
+     * @var string[]
+     */
+    public array $aliases;
+}
