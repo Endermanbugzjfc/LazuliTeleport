7fa030b002f783b57934c15ef8ec5a7315344558
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-21 03:40:50 +0800

    Added function to close channel

--- LazuliTeleport/src/Utils/Utils.php
+++ LazuliTeleport/src/Utils/Utils.php
@@ -4,9 +4,28 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\LazuliTeleport\Utils;
 
+use SOFe\AwaitGenerator\Channel;
+
 final class Utils
 {
     private function __construct()
     {
+    }
+
+    /**
+     * @param Channel<null>|null $channel
+     * @return Channel<null>|null
+     */
+    public static function closeChannel(
+    	?Channel $channel
+    ) : ?Channel {
+    	if ($channel !== null) {
+	    	$queueSize = $channel->getReceiveQueueSize();
+	    	for ($receiver = 0; $receiver < $queueSize; $receiver++) {
+	    		$channel->sendWithoutWait(null);
+	    	}
+    	}
+
+    	return $channel;
     }
 }
