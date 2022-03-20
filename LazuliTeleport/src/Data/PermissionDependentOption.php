7d10469901f098d6322976ac8da1f802668dc45e
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-21 01:42:51 +0800

    Explain why I set the default value to null

--- LazuliTeleport/src/Data/PermissionDependentOption.php
+++ LazuliTeleport/src/Data/PermissionDependentOption.php
@@ -6,6 +6,11 @@ namespace Endermanbugzjfc\LazuliTeleport\Data;
 
 class PermissionDependentOption
 {
+    /*
+     * To anyone who is using ConfigStruct:
+     * Default value of a nullable property should always be null.
+     * Otherwise if the user does not set a value, the default value will be used.
+     */
     public ?int $waitDurationAfterAcceptRequest = null;
     public ?int $tpahereRequesteeLimit = null;
 
