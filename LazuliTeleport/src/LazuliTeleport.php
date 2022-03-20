e58c8def9d79c7cb1deb36794889453658199b3b
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-20 23:18:43 +0800

    Added plugin config setter and getter

--- LazuliTeleport/src/LazuliTeleport.php
+++ LazuliTeleport/src/LazuliTeleport.php
@@ -11,6 +11,7 @@ use pocketmine\permission\Permission;
 use pocketmine\permission\PermissionManager;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+use RuntimeException;
 use function file_exists;
 use function file_put_contents;
 
@@ -23,27 +24,46 @@ class LazuliTeleport extends PluginBase
         self::$instance = $this;
     }
 
+    protected PluginConfig $configObject;
+
+    public function getConfigObject() : PluginConfig
+    {
+        return $this->configObject;
+    }
+
+
+    public function setConfigObject(
+        PluginConfig $configObject
+    ) : void {
+        $this->configObject = $configObject;
+    }
+
+    public function getConfig() : Config
+    {
+        throw new RuntimeException("Use getConfigObject() instead");
+    }
+
     protected function onEnable() : void
     {
-        $config = new PluginConfig();
+        $this->configObject = new PluginConfig();
         $path = $this->getDataFolder() . "config.yml";
         if (!file_exists($path)) {
-            $data = Emit::object($config);
+            $data = Emit::object($this->configObject);
             file_put_contents($path, $data);
         } else {
             $data = (new Config($path))->getAll();
-            $context = Parse::object($config, $data);
-            $context->copyToObject($config, $path);
+            $context = Parse::object($this->configObject, $data);
+            $context->copyToObject($this->configObject, $path);
         }
 
         $pluginName = $this->getName();
         $waitDurationDescription = $pluginName . " wait duration permission";
-        foreach ($config->waitDurationAfterAcceptRequest as $permission => $time) {
+        foreach ($this->configObject->waitDurationAfterAcceptRequest as $permission => $time) {
             $permissionInstances[] = new Permission($permission, $waitDurationDescription);
         }
 
         $requesteeLimitDescription = $pluginName . " tpahere requestee limit permission";
-        foreach ($config->tpahereRequesteeLimit as $permission => $limit) {
+        foreach ($this->configObject->tpahereRequesteeLimit as $permission => $limit) {
             $permissionInstances[] = new Permission($permission, $requesteeLimitDescription);
         }
         foreach ($permissionInstances ?? [] as $permissionInstance) {
