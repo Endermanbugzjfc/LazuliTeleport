f30c7bdd24467ef718543a5cd991cc827635c062
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-21 07:00:02 +0800

    Added player session info

--- LazuliTeleport/src/Player/PlayerSessionInfo.php
+++ LazuliTeleport/src/Player/PlayerSessionInfo.php
@@ -4,16 +4,66 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\LazuliTeleport\Player;
 
+use Endermanbugzjfc\LazuliTeleport\LazuliTeleport;
+use SOFe\InfoAPI\DurationInfo;
 use SOFe\InfoAPI\Info;
+use SOFe\InfoAPI\InfoAPI;
+use SOFe\InfoAPI\NumberInfo;
+use SOFe\InfoAPI\PlayerInfo;
 
 final class PlayerSessionInfo extends Info
 {
-    public int $tpaRequesteeLimit;
-    public int $tpaCoolDown;
+    public function __construct(
+        protected NumberInfo $tpaRequesteeLimit,
+        protected DurationInfo $tpaCoolDown,
 
-    public int $tpahereRequesteeLimit;
-    public int $tpahereCoolDown;
+        protected NumberInfo $tpahereRequesteeLimit,
+        protected DurationInfo $tpahereCoolDown,
 
-    public int $waitDuration;
-    public int $forceWaitDuration;
+        protected DurationInfo $teleportationWaitDuration,
+        protected DurationInfo $forcedTeleportationWaitDuration,
+
+        protected PlayerInfo $playerInfo
+    ) {
+    }
+
+    public function toString() : string
+    {
+        return $this->playerInfo->toString();
+    }
+
+    public static function init() : void
+    {
+        $pluginName = LazuliTeleport::getInstance()->getName();
+        InfoAPI::provideInfo(
+            self::class,
+            NumberInfo::class,
+            "$pluginName.Player.TpaRequesteeLimit",
+            fn(self $info) : NumberInfo => $info->tpaRequesteeLimit
+        );
+        InfoAPI::provideInfo(
+            self::class,
+            DurationInfo::class,
+            "$pluginName.Player.TpaCoolDown",
+            fn(self $info) : DurationInfo => $info->tpaCoolDown
+        );
+        InfoAPI::provideInfo(
+            self::class,
+            NumberInfo::class,
+            "$pluginName.Player.TpahereRequesteeLimit",
+            fn(self $info) : NumberInfo => $info->tpahereRequesteeLimit
+        );
+        InfoAPI::provideInfo(
+            self::class,
+            DurationInfo::class,
+            "$pluginName.Player.TeleportationWaitDuration",
+            fn(self $info) : DurationInfo => $info->teleportationWaitDuration
+        );
+        InfoAPI::provideInfo(
+            self::class,
+            DurationInfo::class,
+            "$pluginName.Player.ForcedTeleportationWaitDuration",
+            fn(self $info) : DurationInfo => $info->forcedTeleportationWaitDuration
+        );
+    }
 }
