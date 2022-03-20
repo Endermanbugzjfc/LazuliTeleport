3fc1e31ce2c1e4737f15863f91ba561330d35dac
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-20 21:29:45 +0800

    Load plugin config

--- LazuliTeleport/src/LazuliTeleport.php
+++ LazuliTeleport/src/LazuliTeleport.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\LazuliTeleport;
 
+use Endermanbugzjfc\ConfigStruct\Parse;
+use Endermanbugzjfc\LazuliTeleport\Data\PluginConfig;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 
 class LazuliTeleport extends PluginBase
 {
@@ -13,6 +16,15 @@ class LazuliTeleport extends PluginBase
     protected function onLoad() : void
     {
         self::$instance = $this;
+    }
+
+    protected function onEnable() : void
+    {
+        $path = $this->getDataFolder() . "config.yml";
+        $data = (new Config($path))->getAll();
+        $config = new PluginConfig();
+        $context = Parse::object($config, $data);
+        $context->copyToObject($config, $path);
     }
 
     public static function getInstance() : self
