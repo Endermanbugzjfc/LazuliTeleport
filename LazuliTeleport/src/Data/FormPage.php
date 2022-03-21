4c41dc724fd95407d470e4c4e7d4331e0f5fd8e0
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-21 09:22:54 +0800

    Added form page struct

---
+++ LazuliTeleport/src/Data/FormPage.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Endermanbugzjfc\LazuliTeleport\Data;
+
+class FormPage {
+	public bool $enabled = true;
+
+	public ?string $title = null;
+
+	public ?string $pageDropdownLabel = null;
+	public ?string $pageFindPlayer = null;
+	public ?string $pageListPlayer = null;
+	public ?string $pageViewTeleportationRequest = null;
+	public ?string $pageExit = null;
+
+	public ?string $playerSearchBarLabel = null;
+	public ?string $playerSearchPlaceholder = null;
+	public ?string $playerSearchDefaultValue = null;
+
+	public ?string $playerToggleLabel = null;
+
+	public ?string $actionSliderLabel;
+	public ?string $actionTpa = null;
+	public ?string $actionTpahere = null;
+	public ?string $actionBlock = null;
+
+	public ?string $forceModeToggleLabel = null;
+
+	public ?string $forceModeWaitDurationSliderLabel = null;
+	public ?int $forceModeWaitDurationMin = null;
+	public ?int $forceModeWaitDurationStep = null;
+	public ?int $forceModeWaitDurationTotalSteps = null;
+
+	public ?string $requestDetails = null;
+
+	public ?string $requestResponseSliderLabel = null;
+	public ?string $requestAccept = null;
+	public ?string $requestReject = null;
+}
