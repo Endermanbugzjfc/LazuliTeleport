6152f7d79a5964437071ac6eb795f63f71e9ccda
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-22 03:33:49 +0800

    Documented this complex function

--- LazuliTeleport/src/LazuliTeleport.php
+++ LazuliTeleport/src/LazuliTeleport.php
@@ -227,6 +227,14 @@ class LazuliTeleport extends PluginBase
         return $command;
     }
 
+    /**
+     * Group refers to {@link PermissionDependentOption} here.
+     * Groups that do not matchc the player's permission will be omitted.
+     * Clone the fallback group or the group with least priority if there is no fallback group. Now this becomes the group instance specifically for this player.
+     * Then, loop through all other groups by ascending priority order. And {@link PermissionDependentOption::override()} the the properties in player-specific group with values in those group.
+     * @param Player $player
+     * @return PermissionDependentOption
+     */
     public function getPlayerOptions(
         Player $player
     ) : PermissionDependentOption {
