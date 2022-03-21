ae1687e7c5c09860552b1ee2cd47904ef07677a4
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-21 09:36:01 +0800

    Extracted commands struct from plugin config

---
+++ LazuliTeleport/src/Data/Commands.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\LazuliTeleport\Data;
+
+use ReflectionClass;
+use ReflectionProperty;
+use RuntimeException;
+
+class Commands
+{
+    public CommandProfile $tpa;
+    public CommandProfile $tpahere;
+    public CommandProfile $tpaccept;
+    public CommandProfile $tpareject;
+    public CommandProfile $tpablock;
+    public CommandProfile $tpaforce;
+
+    /**
+     * @return CommandProfile[]
+     */
+    public static function getDefaultCommandProfiles() : array
+    {
+        $reflection = new ReflectionClass(self::class);
+        $filter = ReflectionProperty::IS_PUBLIC;
+        $properties = $reflection->getProperties($filter);
+        foreach ($properties as $property) {
+            $name = $property->getName();
+            $description = match ($name) {
+                "tpa" => "Request teleportation to another player",
+                "tpahere" => "Request teleporting another player to you",
+                "tpaccept" => "Accept a teleportation request",
+                "tpareject" => "Reject a teleportation request",
+                "tpablock" => "Block another player from sending you teleportation request",
+                "tpaforce" => "(Admin command) Automatically accept teleportation request you send",
+                default => throw new RuntimeException("Unknown command \"$name\"")
+            };
+            $return[] = CommandProfile::create($name, $description);
+        }
+        return $return ?? [];
+    }
+}
--- LazuliTeleport/src/Data/PluginConfig.php
+++ LazuliTeleport/src/Data/PluginConfig.php
@@ -11,12 +11,6 @@ use function uasort;
 class PluginConfig
 {
 
-    /**
-     * @var CommandProfile[]
-     */
-    #[ListType(CommandProfile::class)]
-    public array $commands;
-
     public int $requestTimeout = 60;
 
     public bool $repeatDurationMessage = false;
@@ -35,61 +29,8 @@ class PluginConfig
     public function __construct()
     {
         $pluginName = LazuliTeleport::getInstance()->getName();
-        $this->commands = self::getDefaultCommandProfiles();
         $this->permissionDependentOptions[""] =
         $this->permissionDependentOptions["$pluginName.group.name"] = PermissionDependentOption::getDefault();
-    }
-
-    /**
-     * @return CommandProfile[]
-     */
-    public static function getDefaultCommandProfiles() : array
-    {
-        foreach ([
-            CommandProfile::create(
-                "tpa",
-                "Request teleportation to another player"
-            ),
-            CommandProfile::create(
-                "tpahere",
-                "Request teleporting another player to you",
-                [
-                    "tphere"
-                ]
-            ),
-            CommandProfile::create(
-                "tpaccept",
-                "Accept a teleportation request"
-            ),
-            CommandProfile::create(
-                "tpareject",
-                "Reject a teleportation request",
-                [
-                    "tpreject",
-                    "tpadeny",
-                    "tpdeny"
-                ]
-            ),
-            CommandProfile::create(
-                "tpablock",
-                "Block another player from sending you teleportation request",
-                [
-                    "tpblock"
-                ]
-            ),
-            CommandProfile::create(
-                "tpaforce",
-                "(Admin command) Automatically accept teleportation request you send",
-                [
-                    "tpforce"
-                ]
-            )
-        ] as $profile) {
-            $name = $profile->name;
-            $return[$name] = $profile;
-        }
-
-        return $return;
     }
 
     /**
