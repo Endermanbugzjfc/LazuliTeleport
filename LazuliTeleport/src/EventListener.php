81a29bbc8ea935ac03c4ddbbda98b4fa7c0c7c13
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-21 08:15:59 +0800

    Manage player sessions

--- LazuliTeleport/src/EventListener.php
+++ LazuliTeleport/src/EventListener.php
@@ -5,11 +5,18 @@ declare(strict_types=1);
 namespace Endermanbugzjfc\LazuliTeleport;
 
 use Endermanbugzjfc\LazuliTeleport\Player\PlayerSession;
+use Generator;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerLoginEvent;
+use SOFe\AwaitGenerator\Await;
 
 class EventListener implements Listener
 {
+    /**
+     * @var PlayerSession[]
+     * @phpstan-var array<string, PlayerSession> Key = player's unique ID in 16 bytes.
+     */
+    public $playerSessions = [];
 
     /**
      * This listener is for opening player sessions.
@@ -18,7 +25,15 @@ class EventListener implements Listener
     public function onPlayerLoginEvent(
         PlayerLoginEvent $event
     ) : void {
-        $player = $event->getPlayer();
-        $session = new PlayerSession($player);
+        Await::f2c(function() use ($event) : Generator {
+            $player = $event->getPlayer();
+            $arrayKey = "";
+            yield from Await::promise(function($then, $catch) use ($player, &$arrayKey) {
+                $session = new PlayerSession($player, $then);
+                $arrayKey = $session->arrayKey();
+                $this->playerSessions[$arrayKey] = $session;
+            });
+            unset($this->playerSessions[$arrayKey]);
+        });
     }
 }
