984b536732a333351cbf4ea73cc8ab5f3cc71e17
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-21 10:16:41 +0800

    Load form contents

---
+++ LazuliTeleport/src/Data/Form.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endermanbugzjfc\LazuliTeleport\Data;
+
+class Form
+{
+    public FormPage $fallback;
+    public FormPage $findPlayer;
+    public FormPage $listPlayer;
+    public FormPage $pageViewTeleportationRequest;
+}
