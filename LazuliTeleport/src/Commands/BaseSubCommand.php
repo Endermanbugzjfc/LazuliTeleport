4b677313ed4b5d60e4a07ec746f60602e3c907e9
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-22 18:41:31 +0800

    Forgot to add namespace

--- LazuliTeleport/src/Commands/BaseSubCommand.php
+++ LazuliTeleport/src/Commands/BaseSubCommand.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace Endermanbugzjfc\LazuliTeleport\Commands;
+
 use CortexPE\Commando\BaseSubCommand as CommandoBaseSubCommand;
 
 abstract class BaseSubCommand extends CommandoBaseSubCommand
