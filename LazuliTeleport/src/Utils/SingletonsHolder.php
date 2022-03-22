4ab7294559889eafd69e320baa3f83953de8b544
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-23 07:15:59 +0800

    Allow waiting for a singleton to be registered

--- LazuliTeleport/src/Utils/SingletonsHolder.php
+++ LazuliTeleport/src/Utils/SingletonsHolder.php
@@ -1,33 +1,58 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Endermanbugzjfc\LazuliTeleport\Utils;
 
-class SingletonsHolder {
+use Generator;
+use SOFe\AwaitGenerator\Channel;
 
-	/**
-	 * @var array<class-string, object>
-	 */
-	protected array $singletons = [];
+class SingletonsHolder
+{
+    /**
+     * @var array<class-string, object>
+     */
+    protected array $singletons = [];
 
-	public function register(
-		object $singleton
-	) : void {
-		$class = $singleton::class;
-		$this->singletons[$class] = $singleton;
-	}
+    public function register(
+        object $singleton
+    ) : void {
+        $class = $singleton::class;
+        $this->singletons[$class] = $singleton;
+        $waitFor = $this->queue[$class] ?? null;
+        Utils::closeChannel($waitFor, $singleton);
+    }
 
-	/**
-	 * @template T of object
-	 * @param class-string<T> $class
-	 * @return T
-	 */
-	public function get(
-		string $class
-	) : object {
-		/**
-		 * @var T
-		 */
-		$object = $this->singletons[$class];
-		return $object;
-	}
+    /**
+     * @template T of object
+     * @param class-string<T> $class
+     * @return T
+     */
+    public function get(
+        string $class
+    ) : object {
+        /**
+         * @var T
+         */
+        $object = $this->singletons[$class];
+        return $object;
+    }
+
+    /**
+     * @var Channel<object>[]
+     * @phpstan-var array<class-string, Channel<object>>
+     */
+    protected array $queue;
+
+    /**
+     * @template T of object
+     * @param class-string<T> $class
+     * @return Generator<mixed, mixed, mixed, T>
+     */
+    public function waitFor(
+        string $class
+    ) : Generator {
+        $this->queue[$class] = $channel = new Channel();
+        return yield from $channel->receive();
+    }
 }
