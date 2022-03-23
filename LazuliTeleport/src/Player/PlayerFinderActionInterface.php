f7853cf02437b367d5c8cb563c34ab2b307e54e4
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-23 12:58:31 +0800

    Try to put more on the action interface rather than player finder itself

--- LazuliTeleport/src/Player/PlayerFinderActionInterface.php
+++ LazuliTeleport/src/Player/PlayerFinderActionInterface.php
@@ -4,14 +4,25 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\LazuliTeleport\Player;
 
+use Generator;
 use Ramsey\Uuid\UuidInterface;
 
 interface PlayerFinderActionInterface
 {
-    public function getMaxTargetsLimit() : int;
-
     public function runWithSelectedTargets(
         PlayerSession $session,
         UuidInterface ...$targets
     ) : void;
+
+    public function getActionDisplayName(
+        PlayerSession $session
+    ) : string;
+
+    /**
+     * @return Generator<mixed, mixed, mixed, bool>
+     */
+    public function isActionAvailable(
+        PlayerSession $session,
+        UuidInterface ...$targets
+    ) : Generator;
 }
