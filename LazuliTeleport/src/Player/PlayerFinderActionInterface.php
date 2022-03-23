4656adb4ad81b4009b0b6552944bf6edfb606c57
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-24 07:09:08 +0800

    Revert "Added get instance in action interface"
    This reverts commit b4e40476443ebe0afb54bb476bbdd4cad61837b2.

--- LazuliTeleport/src/Player/PlayerFinderActionInterface.php
+++ LazuliTeleport/src/Player/PlayerFinderActionInterface.php
@@ -10,6 +10,7 @@ use Ramsey\Uuid\UuidInterface;
 interface PlayerFinderActionInterface
 {
     /**
+     * @param PlayerSession $session
      * @param string ...$targets Player names. Case might not be accurate. All offline player's names are in lowercase.
      */
     public function runWithSelectedTargets(
@@ -28,9 +29,4 @@ interface PlayerFinderActionInterface
         PlayerSession $session,
         UuidInterface ...$targets
     ) : Generator;
-
-    /**
-     * @return Generator<mixed, mixed, mixed, static>
-     */
-    public static function getInstance() : Generator;
 }
