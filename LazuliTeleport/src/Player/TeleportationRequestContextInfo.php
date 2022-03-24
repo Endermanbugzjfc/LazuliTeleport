1b94c5087999c51d0f6591834900e29f56db6df4
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-24 15:12:24 +0800

    Allow teleportation request to be offline (updated its info)

--- LazuliTeleport/src/Player/TeleportationRequestContextInfo.php
+++ LazuliTeleport/src/Player/TeleportationRequestContextInfo.php
@@ -5,26 +5,33 @@ declare(strict_types=1);
 namespace Endermanbugzjfc\LazuliTeleport\Player;
 
 use Endermanbugzjfc\LazuliTeleport\LazuliTeleport;
+use pocketmine\Server;
 use RuntimeException;
 use SOFe\InfoAPI\CommonInfo;
 use SOFe\InfoAPI\DurationInfo;
 use SOFe\InfoAPI\Info;
 use SOFe\InfoAPI\InfoAPI;
-use pocketmine\Server;
+use SOFe\InfoAPI\PositionInfo;
+use SOFe\InfoAPI\StringInfo;
+use SOFe\InfoAPI\TimeInfo;
 
 final class TeleportationRequestContextInfo extends Info
 {
     public function __construct(
-        protected PlayerSessionInfo $requestor,
-        protected PlayerSessionInfo $requestee,
+        protected StringInfo $requestor,
+        protected StringInfo $requestee,
+        protected PositionInfo $requestorPosition,
+        protected PositionInfo $requesteePosition,
+        protected TimeInfo $requestSendTime,
         protected DurationInfo $teleportationWaitDuration,
 
         protected bool $blocked
     ) {
     }
 
-    public function getBlocked() : bool {
-    	return $this->blocked;
+    public function getBlocked() : bool
+    {
+        return $this->blocked;
     }
 
     public function toString() : string
@@ -32,30 +39,55 @@ final class TeleportationRequestContextInfo extends Info
         throw new RuntimeException(self::class . " must not be returned as a provided info");
     }
 
-    public static function init() : void {
-    	$pluginName = LazuliTeleport::getInstance()->getName();
-    	InfoAPI::provideInfo(
-    		self::class,
-    		PlayerSessionInfo::class,
-    		"$pluginName.TeleportationRequest.Requestor",
-    		fn(self $info) : PlayerSessionInfo => $info->requestor
-    	);
-    	InfoAPI::provideInfo(
-    		self::class,
-    		PlayerSessionInfo::class,
-    		"$pluginName.TeleportationRequest.Requestee",
-    		fn(self $info) : PlayerSessionInfo => $info->requestor
-    	);
-    	InfoAPI::provideInfo(
-    		self::class,
-    		DurationInfo::class,
-    		"$pluginName.TeleportationRequest.TeleportationWaitDuration",
-    		fn(self $info) : DurationInfo => $info->teleportationWaitDuration
-    	);
-    	InfoAPI::provideFallback(
-    		self::class,
-    		CommonInfo::class,
-    		fn(self $info) : CommonInfo => new CommonInfo(Server::getInstance())
-    	);
+    public static function init() : void
+    {
+        $pluginName = LazuliTeleport::getInstance()->getName();
+        InfoAPI::provideInfo(
+            self::class,
+            StringInfo::class,
+            "$pluginName.TeleportationRequest.Requestor",
+            fn(self $info) : StringInfo => $info->requestor
+        );
+        InfoAPI::provideInfo(
+            self::class,
+            StringInfo::class,
+            "$pluginName.TeleportationRequest.Requestee",
+            fn(self $info) : StringInfo => $info->requestor
+        );
+        InfoAPI::provideInfo(
+            self::class,
+            PositionInfo::class,
+            "$pluginName.TeleportationRequest.RequesteePosition",
+            fn(self $info) : PositionInfo => $info->requestorPosition
+        );
+        InfoAPI::provideInfo(
+            self::class,
+            PositionInfo::class,
+            "$pluginName.TeleportationRequest.RequesteePosition",
+            fn(self $info) : PositionInfo => $info->requestorPosition
+        );
+        InfoAPI::provideInfo(
+            self::class,
+            PositionInfo::class,
+            "$pluginName.TeleportationRequest.RequesteePosition",
+            fn(self $info) : PositionInfo => $info->requestorPosition
+        );
+        InfoAPI::provideInfo(
+            self::class,
+            TimeInfo::class,
+            "$pluginName.TeleportationRequest.RequestSendTime",
+            fn(self $info) : TimeInfo => $info->requestSendTime
+        );
+        InfoAPI::provideInfo(
+            self::class,
+            DurationInfo::class,
+            "$pluginName.TeleportationRequest.TeleportationWaitDuration",
+            fn(self $info) : DurationInfo => $info->teleportationWaitDuration
+        );
+        InfoAPI::provideFallback(
+            self::class,
+            CommonInfo::class,
+            fn(self $info) : CommonInfo => new CommonInfo(Server::getInstance())
+        );
     }
 }
