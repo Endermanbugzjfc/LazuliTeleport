6de73f02a5e1424daa1f4589908b71f45bd2edb7
Endermanbugzjfc <53002741+Endermanbugzjfc@users.noreply.github.com>
2022-03-24 21:08:24 +0800

    Allow player finding system to be used in non-in-game commands

--- LazuliTeleport/src/Commands/PlayerNotFoundException.php
+++ LazuliTeleport/src/Commands/PlayerNotFoundException.php
@@ -4,30 +4,50 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\LazuliTeleport\Commands;
 
+use Endermanbugzjfc\LazuliTeleport\Data\Messages;
 use Endermanbugzjfc\LazuliTeleport\LazuliTeleport;
 use Endermanbugzjfc\LazuliTeleport\Player\PlayerFinderActionInterface;
 use Endermanbugzjfc\LazuliTeleport\Player\PlayerSession;
 use pocketmine\command\CommandSender;
+use pocketmine\Server;
 use SOFe\InfoAPI\AnonInfo;
+use SOFe\InfoAPI\CommonInfo;
+use SOFe\InfoAPI\InfoAPI;
 use SOFe\InfoAPI\StringInfo;
 
 class PlayerNotFoundException extends TerminateCommandException
 {
-    protected PlayerSession $session;
+    protected ?PlayerSession $session = null;
 
     protected PlayerFinderActionInterface $action;
 
     protected string $input;
 
+    protected string $infoNamespace;
+
     public static function create(
-        PlayerSession $session,
+        ?PlayerSession $session = null,
         PlayerFinderActionInterface $action,
         string $input
     ) : self {
-        $self = new self("Player not found");
+        $pluginName = LazuliTeleport::getInstance()->getName();
+        $infoNamespace = "$pluginName.CommandPlayerFinder";
+        $consoleMessage = Messages::getDefault()->playerNotFound;
+        $consoleInfo = new class(
+            $infoNamespace,
+            [
+                "Input" => new StringInfo($input)
+            ],
+            [
+                new CommonInfo(Server::getInstance())
+            ]
+        ) extends AnonInfo {
+        };
+        $self = new self(InfoAPI::resolve($consoleMessage->chat ?? "", $consoleInfo));
         $self->session = $session;
         $self->action = $action;
         $self->input = $input;
+        $self->infoNamespace = $infoNamespace;
 
         return $self;
     }
@@ -36,11 +56,15 @@ class PlayerNotFoundException extends TerminateCommandException
         CommandSender $sender
     ) : void {
         $session = $this->session;
+        if ($session === null) {
+            parent::handle($sender);
+            return;
+        }
         $message = $session->getMessages()->playerNotFound;
         $pluginName = LazuliTeleport::getInstance()->getName();
         $input = $this->input;
         $info = new class(
-            "$pluginName.CommandPlayerFinder",
+            $this->infoNamespace,
             [
                 "Input" => new StringInfo($input)
             ],
